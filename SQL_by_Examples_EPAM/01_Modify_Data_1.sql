fc0931486450e286f0782772f6acc27b4e359ead
Olena Babko <elena.babko@gmail.com>
2024-01-16 09:22:43 +0200

    count subscribers names

--- SQL_by_Examples_EPAM/01_Modify_Data_1.sql
+++ SQL_by_Examples_EPAM/01_Modify_Data_1.sql
@@ -6,3 +6,11 @@
 SELECT DISTINCT sb_subscriber
 FROM subscriptions;
 
+# 2 Show the list of all subscribers, with counting their names
+SELECT s_name,
+ COUNT(*) AS people_count
+FROM subscribers
+GROUP BY s_name;
+
+
+
