01287f7d4636129910e6bf63d957c4c8a39eb01f
Olena Babko <elena.babko@gmail.com>
2024-06-05 14:02:58 +0300

    queries to show all books written by Alexander Pushkin and/or Isaac Asimov (either individually or as co-authors);

--- SQL_by_Examples_EPAM/01_Modify_Data_2.sql
+++ SQL_by_Examples_EPAM/01_Modify_Data_2.sql
@@ -334,3 +334,13 @@ JOIN m2m_books_authors USING(b_id)
 JOIN authors USING(a_id)
 WHERE a_name IN ("Alexander Pushkin", "Isaac Asimov");
 
+# OR
+SELECT b_name
+FROM books
+JOIN m2m_books_authors USING(b_id)
+WHERE a_id IN (
+	SELECT a_id
+    FROM authors
+    WHERE a_name IN ("Alexander Pushkin", "Isaac Asimov")
+);
+
